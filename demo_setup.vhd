77173fe894e47d527580feefbe337d757ad6e806
Gustavo Fernandez <gustavo.fernandez.costa@gmail.com>
2017-06-21 01:36:26 -0300

    Ta perfeito

--- demo_setup.vhd
+++ demo_setup.vhd
@@ -9,7 +9,7 @@ use work.test_package.all;
 entity demo_setup is
 -- winner: define o vencedor da rodada
 	port (SW			: IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-			GPIO_1IN : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+			GPIO_1IN : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
 			GPIO_1OUT : OUT STD_LOGIC_VECTOR(5 DOWNTO 0);
 			CLOCK_50 : in std_logic;
 			------------------------	Clock Input	 	------------------------
@@ -39,14 +39,16 @@ architecture behavior of demo_setup is
 	signal w	:	std_logic :='0';
 	signal state : state_type;
 	signal mouse_buttons	: std_logic_vector(2 downto 0);
-	signal wheel_action, Placar, screen: std_logic_vector(1 downto 0);
+	signal wheel_action, PlacarP1, PlacarP2, screen: std_logic_vector(1 downto 0);
 	signal number_input : std_logic_vector(2 downto 0);
+	--signal opt_guess	: std_logic_vector(2 downto 0);
 begin
 	Choice <= GPIO_1IN(0);
 	Guess <= GPIO_1IN(1);
-	Placar <= GPIO_1IN(3 downto 2);
-	Fim <= GPIO_1IN(4);
-	Winner <= GPIO_1IN(5);
+	PlacarP1 <= GPIO_1IN(3 downto 2);
+	PlacarP2 <= GPIO_1IN(5 downto 4);
+	Fim <= GPIO_1IN(9);
+	Winner <= GPIO_1IN(10);
 	w <= Choice xor Guess;
 	screen <= Guess & Choice;
 	LEDG(7) <= En;
@@ -82,5 +84,5 @@ begin
 	this_user: user PORT MAP (Choice, Guess, En, '0', reset, "111", wheel_action, mouse_buttons, number_input,
 									  GPIO_1OUT(1 downto 0), GPIO_1OUT(2), GPIO_1OUT(5 downto 3));
 	
-	monitor1	: test PORT MAP (mouse_buttons(1), Placar, SW(6 downto 5), number_input, screen, CLOCK_24(0), KEY(2), VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS);	
+	monitor1	: test PORT MAP (mouse_buttons(1), PlacarP1, PlacarP2, number_input, screen, CLOCK_24(0), KEY(2), VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS);	
 end behavior;
